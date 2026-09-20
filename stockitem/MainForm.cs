7fe2cdb08aba0abf3465d7a7557ef1616273df94
nisanmgr718-lgtm <nisanmgr718@gmail.com>
2026-09-20 14:21:17 +0545

    Fix build errors and confirm app runs

--- stockitem/MainForm.cs
+++ stockitem/MainForm.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace StockWise
 {
--- stockitem/Program.cs
+++ stockitem/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace StockWise
 {
